dc03bf900215e488b12e9b34c5518fe2d7ff11a0
vyshnavi515 <54313325+vyshnavi515@users.noreply.github.com>
2019-09-11 15:44:41 +0530

    Add files via upload

---
+++ mymod.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def welcome_note(name):
+    return 'welcome to the python programming calss '+name+' we wish u all the best'
+
+
+# In[ ]:
+
+
+
+
---
+++ mymodl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def welcome_note(name):
+    return 'welcome to the python programming calss '+name+' we wish u all the best'
+
+
+# In[1]:
+
+
+def addition(a,b):
+    return a+b
+
+
+# In[2]:
+
+
+def subt(a,b):
+    return a-b
+
+
+# In[4]:
+
+
+def divi(a,b):
+    if b==0:
+        print('ZeroDivisionError: division by zero')
+        return
+    else:
+        return a/b
+    
+
+
+# In[5]:
+
+
+def mult(a,b):
+    return a*b
+
+
+# In[ ]:
+
+
+
+
